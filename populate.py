fdaed92350d552ad8c0ea44125f76f85cb8a38fd
TarushShankar <tarush@ualberta.ca>
2024-12-03 00:53:06 -0700

    Created a code to automate generating a table

--- populate.py
+++ populate.py
@@ -9,18 +9,18 @@ def populate_csv():
 
     for country in os.listdir(main_folder):
         country_path = os.path.join(main_folder,country)
+        print("We are in Country:",country,"\n")
         for file in os.listdir(country_path):
-            print("For Country:",country,"\n")
             if file.endswith('.txt'):
                 file_path = os.path.join(country_path,file)
-                header = file_path.split('_Results')[0]
+                header = file.split('_Results')[0]
                 
-                with open(file_path,'r'):
-                    data = file.splitlines()
-                    print(data)
+                with open(file_path,'r') as file:
+                    data = file.readlines()
+                    #print(data)
                     try:
-                        mse = data[-2].split(': ')[-1]
-                        r2_score = data[-1].split(': ')[-1]
+                        mse = data[-3].split(': ')[-1]
+                        r2_score = data[-2].split(': ')[-1]
 
                         result = {'Country':country,'Model':header,'MSE':mse,'R2':r2_score}
                         results.append(result)
@@ -28,6 +28,15 @@ def populate_csv():
                         print(e)
 
     print(results)
+
+    results_df = pd.DataFrame(results)
+    csv_file = "COVID_model_results.csv"
+    directory = os.path.join(main_folder,csv_file)
+
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    results_df.to_csv(directory, index=False)
 
 
 if __name__ == "__main__":
